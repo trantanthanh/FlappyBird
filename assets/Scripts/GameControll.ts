22da8231031e11aa2dae90fc7fe4a8636a436e92
Thành Trần Tấn <trantanthanh@gmail.com>
2023-11-06 15:44:01 +0700

    update game control for show, hide result score

--- assets/Scripts/GameControll.ts
+++ assets/Scripts/GameControll.ts
@@ -1,5 +1,6 @@
-import { _decorator, Component, Node, CCInteger } from 'cc';
+import { _decorator, Component, Node, CCInteger, input, Input, EventKeyboard, KeyCode } from 'cc';
 import { Ground } from './Ground';
+import { Results } from './Results';
 const { ccclass, property } = _decorator;
 
 @ccclass('GameControll')
@@ -9,6 +10,11 @@ export class GameControll extends Component {
     }) ground: Ground;
 
     @property({
+        type: Results,
+        tooltip: "this is results"
+    }) results: Results;
+
+    @property({
         type: CCInteger
     }) gameSpeed: number = 300;
 
@@ -16,8 +22,32 @@ export class GameControll extends Component {
         type: CCInteger
     }) pipeSpeed: number = 200;
 
-    onLoad() { }
-    initListener() { }
+    onLoad() { 
+        this.initListener();
+    }
+    initListener() {
+        input.on(Input.EventType.KEY_DOWN, this.onKeyDown, this);
+    }
+
+    onKeyDown(event: EventKeyboard) {
+        switch (event.keyCode) {
+            case KeyCode.KEY_A:
+                {
+                    this.GameOver();
+                    break;
+                }
+            case KeyCode.KEY_P:
+                {
+                    this.results.AddScore();
+                    break;
+                }
+        }
+    }
+    GameOver() {
+        this.results.ShowResult();
+    }
+
+    onTouchMove(event: EventKeyboard) { }
 }
 
 
--- assets/Scripts/Results.ts
+++ assets/Scripts/Results.ts
@@ -6,17 +6,46 @@ export class Results extends Component {
     @property({
         type: Label,
         tooltip: 'this is current score'
-    }) currentScore: Label;
+    }) scoreLabel: Label;
 
     @property({
         type: Label,
         tooltip: 'this is high score'
-    }) highScore: Label;
+    }) highScoreLabel: Label;
 
     @property({
-        type : Label,
-        tooltip : 'this is try again'
-    }) tryAgain : Label;
+        type: Label,
+        tooltip: 'this is try again'
+    }) tryAgain: Label;
+
+    currentScore: number = 0;
+    maxScore: number = 0;
+
+    UpdateScore(score: number) {
+        this.currentScore = score;
+        this.scoreLabel.string = this.currentScore.toString();
+    }
+
+    ResetScore() {
+        this.UpdateScore(0);
+        this.HideResult();
+    }
+
+    AddScore() {
+        this.UpdateScore(++this.currentScore);
+    }
+
+    ShowResult() {
+        this.maxScore = Math.max(this.maxScore, this.currentScore);
+        this.highScoreLabel.string = 'High Score: ' + this.maxScore.toString();
+        this.tryAgain.node.active = true;
+        this.highScoreLabel.node.active = true;
+    }
+
+    HideResult() {
+        this.tryAgain.node.active = false;
+        this.highScoreLabel.node.active = false;
+    }
 }
 
 
